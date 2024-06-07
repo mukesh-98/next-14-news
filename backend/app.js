1369c2df911758457d23499743f7484e3d5d4d0c
mukesh-98 <parmarmukesh1998.mp.mp@gmail.com>
2024-06-07 18:56:27 +0530

    added backed

---
+++ backend/app.js
@@ -0,0 +1,81 @@
+import express from "express";
+import sqlite from "better-sqlite3";
+import cors from "cors";
+import { data as DUMMY_NEWS } from "./Sample_Report.js";
+const db = sqlite("data.db");
+function initDb() {
+  db.prepare(
+    `CREATE TABLE IF NOT EXISTS news (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    link TEXT,
+    keywords TEXT  NULL,
+    creator TEXT  NULL,
+    video_url TEXT NULL,
+    description TEXT  NULL,
+    content TEXT  NULL,
+    pubDate TEXT  NULL,
+    full_description TEXT  NULL,
+    image_url TEXT  NULL,
+    source_id TEXT  NULL,
+    country TEXT  NULL,
+    category TEXT  NULL,
+    language TEXT  NULL
+)`
+  ).run();
+
+  const { count } = db.prepare("SELECT COUNT(*) as count FROM news").get();
+
+  if (count === 0) {
+    const insert = db.prepare(
+      `INSERT INTO news (
+        title, 
+        link, 
+        keywords, 
+        creator, 
+        video_url, 
+        description, 
+        content, 
+        pubDate, 
+        full_description, 
+        image_url, 
+        source_id, 
+        country, 
+        category, 
+        language
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+    );
+
+    DUMMY_NEWS.forEach((news) => {
+      insert.run(
+        news.title,
+        news.link,
+        JSON.stringify(news.keywords), // serialize array to JSON string
+        JSON.stringify(news.creator),
+        news.video_url,
+        news.description,
+        news.content,
+        news.pubDate,
+        news.full_description,
+        news.image_url,
+        news.source_id,
+        JSON.stringify(news.country),
+        JSON.stringify(news.category),
+        news.language
+      );
+    });
+  }
+}
+
+const app = express();
+
+app.use(cors());
+
+app.get("/news", (req, res) => {
+  const news = db.prepare("SELECT * FROM news").all();
+  res.json(news);
+});
+
+initDb();
+
+app.listen(4001);
